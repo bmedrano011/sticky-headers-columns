d51c87789c673bf70a6f905047e1681087ddcc99
bmedrano011 <51801134+bmedrano011@users.noreply.github.com>
2022-12-10 19:34:14 -0500

    fix: container height prop

--- src/components/Table/TblContainer/index.js
+++ src/components/Table/TblContainer/index.js
@@ -1,9 +1,9 @@
 import { Table, TableContainer } from "@mui/material";
 import React from "react";
 
-function TblContainer({ children, size }) {
+function TblContainer({ children, size, maxHeight }) {
   return (
-    <TableContainer sx={{ maxHeight: 650 }}>
+    <TableContainer sx={{ maxHeight: maxHeight ? maxHeight : 650 }}>
       <Table stickyHeader size={size ? size : "small"}>
         {children}
       </Table>
